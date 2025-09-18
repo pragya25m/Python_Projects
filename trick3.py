a4a9eca8ae32f43dfb3201d1f02c7d16bb688e81
pragya25m <pragyamishra8364@gmail.com>
2025-09-18 12:26:50 +0530

    Update trick3.py

--- trick3.py
+++ trick3.py
@@ -3,4 +3,5 @@ for i in range(1,255):
     ip="192.168.1."+str(i)
     print("Pinging ",ip,".....")
     os.system("ping "+ip)
-print("Finished Pinging, THANK YOU!!")
+
+print("Finished Pinging!!!")
